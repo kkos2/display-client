625c9d68cece6c6a23712340efeafd3020f5d1c3
Mads H. Danquah <mads@danquah.dk>
2023-05-30 09:24:00 +0200

    Align to coding standards

--- src/region.js
+++ src/region.js
@@ -59,7 +59,11 @@ function Region({ region }) {
   function slideDone(slide) {
     const nextSlideAndIndex = findNextSlide(slide.executionId);
 
-    if (nextSlideAndIndex.nextIndex === 0 && Array.isArray(newSlides) && newSlides.length > 0) {
+    if (
+      nextSlideAndIndex.nextIndex === 0 &&
+      Array.isArray(newSlides) &&
+      newSlides.length > 0
+    ) {
       const nextSlides = [...newSlides];
       setSlides(nextSlides);
       setNewSlides(null);
